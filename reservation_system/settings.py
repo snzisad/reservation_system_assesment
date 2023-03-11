289e6a3b69b579428efdc820e56464faff40e46d
snzisad <snzisad@gmail.com>
2023-03-12 00:39:26 +0100

    data retrival SQL quary

--- reservation_system/settings.py
+++ reservation_system/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "" # Enter your secret key
+# SECRET_KEY = "%y6ugzp^+h1vdsrvczg)5tuvroi7(q+6tpfq0zwp500=)wt)=#" # Enter your secret key
+
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
